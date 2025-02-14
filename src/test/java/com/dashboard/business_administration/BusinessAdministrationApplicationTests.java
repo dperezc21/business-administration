a050b8c4532ae3969564df34fb6afb6aeaf1c0e2
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-02-14 16:58:37 -0500

    feat: first commit

---
+++ src/test/java/com/dashboard/business_administration/BusinessAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.business_administration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
